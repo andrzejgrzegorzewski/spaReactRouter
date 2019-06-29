7f2d54501dc5b3654494e0474a0e22a880022b80
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-06-29 21:19:51 +0200

    Updates

---
+++ precache-manifest.82529fe5ba08ff188bb963f34da8806c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/spaReactRouter/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/spaReactRouter/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/spaReactRouter/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "ed0d6bf61f3eba930f64",
+    "url": "/spaReactRouter/static/js/runtime~main.ed0d6bf6.js"
+  },
+  {
+    "revision": "f62d42e3f044a9e1b2a2",
+    "url": "/spaReactRouter/static/js/main.f62d42e3.chunk.js"
+  },
+  {
+    "revision": "0a53d94626351fd9971d",
+    "url": "/spaReactRouter/static/js/2.0a53d946.chunk.js"
+  },
+  {
+    "revision": "f62d42e3f044a9e1b2a2",
+    "url": "/spaReactRouter/static/css/main.3464f9fd.chunk.css"
+  },
+  {
+    "revision": "219029ce507c4025ab6afbe0a86f9b32",
+    "url": "/spaReactRouter/index.html"
+  }
+];
